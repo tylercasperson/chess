bad95a3369e3bbf8f087bbadd3f8e8b5875209d9
Tyler Casperson <tylercasperson@gmail.com>
2020-06-16 22:16:20 -0600

    centered legend stylesheet, added reset style sheet

--- index.php
+++ index.php
@@ -4,21 +4,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Chess</title>
-    <link rel="stylesheet" type="text/css" href="./assets/css/reset.css">
-    <link rel="stylesheet" type="text/css" href="./assets/css/style.css">
+    <link rel="stylesheet" type="text/css" href="./assets/css/reset.css" />
+    <link rel="stylesheet" type="text/css" href="./assets/css/style.css?" />
 </head>
 <body>
     <table>
+        <tbody>
         <?php
             $xAxis = array('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h');
             $yAxis = array('8', '7', '6', '5', '4', '3', '2', '1');
           
             echo "<tr>";
-            for($y=0;$y<8;$y++){
-                if($y==0){
+            for($x=0;$x<8;$x++){
+                if($x==0){
                     echo "<td></td>";
                 }
-                echo "<td class='legend'>" . $yAxis[$y] . "</td>";
+                echo "<td class='xLegend'>" . $xAxis[$x] . "</td>";
             }
             echo "</tr>";
 
@@ -26,7 +27,7 @@
                 echo "<tr class='row'>"; 
                 for($column = 0; $column <= 7; $column++){
                     if($column == 0){
-                        echo "<td class='legend'>" . $xAxis[(7-$row)] . "</td>";
+                        echo "<td class='yLegend'>" . $yAxis[($row)] . "</td>";
                     }
                     if(($row + $column) % 2 == 0){
                         echo "<td id='" . $xAxis[7-$row] . $yAxis[$column] . "' class='cell dark'></td>";
@@ -37,6 +38,7 @@
                 echo "</tr>";
             } 
         ?>
+        </tbody>
     </table>
 </body>
 </html>
